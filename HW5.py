0312f85e4c69f2bbf03b6a76de2ac8ccdd92a160
Lauren Tahari <taharil@umich.edu>
2017-10-04 18:40:23 -0400

    updated hw5

---
+++ HW5.py
@@ -0,0 +1,12 @@
+import re
+
+total= 0
+number = 0
+f = open('regex_sum_38192.txt')
+for line in f:
+    num = re.findall("[0-9]+", line)
+    for i in num:
+        number = number +int(i)
+    total = total + number
+    number = 0
+print (total)
